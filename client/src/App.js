c40875e06c7e24d255e45d0cdd041e53ead73465
Dave <daveroymutisya2@gmail.com>
2024-03-18 20:09:12 +0300

    Running tests and Praticing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [flicks, setFlicks] = useState([]);
+
+  useEffect(() => {
+    fetch("/flicks")
+      .then((res) => res.json())
+      .then((data) => {
+        setFlicks(data.flick);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching flicks:", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {flicks.map((flick, i) => (
+        <p key={i}>{flick}</p>
+      ))}
     </div>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route
+@app.route("/flicks")
+
+def index():
+    return {"flick"  : ["Flick1", "Flick2", "Flick3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)

953918cb7f1e6a0ef3755fcfeebd04c43c664823
Dave <daveroymutisya2@gmail.com>
2024-03-28 19:03:58 +0300

    Fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [flicks, setFlicks] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
 
   useEffect(() => {
-    fetch("/movies") // Adjust the endpoint here based on your Flask routes
+    // Fetch movies
+    fetch("/movies")
       .then((res) => res.json())
       .then((data) => {
-        setFlicks(data); // Set the fetched data directly
-        console.log(data);
+        setMovies(data); // Set the fetched movies
       })
       .catch((error) => {
-        console.error("Error fetching flicks:", error);
+        console.error("Error fetching movies:", error);
+      });
+
+    // Fetch series
+    fetch("/series")
+      .then((res) => res.json())
+      .then((data) => {
+        setSeries(data); // Set the fetched series
+      })
+      .catch((error) => {
+        console.error("Error fetching series:", error);
       });
   }, []);
 
   return (
     <div>
-      {flicks.map((flick, i) => (
-        <p key={i}>{flick.title}</p> {/* Assuming flicks have a 'title' attribute */}
-      ))}
+      <h2>Movies</h2>
+      <div>
+        {movies.map((movie, i) => (
+          <p key={i}>{movie.title}</p>
+        ))}
+      </div>
+      <h2>Series</h2>
+      <div>
+        {series.map((serie, i) => (
+          <p key={i}>{serie.title}</p>
+        ))}
+      </div>
     </div>
   );
 }
